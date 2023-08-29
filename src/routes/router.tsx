5e40bd594879458e94c8eacdf33e4a0ab3cdf81c
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-08-29 17:02:48 +1000

    feat: Added more stuff to Buttons.

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -5,10 +5,11 @@ import {
 } from "react-router-dom";
 import App from "../App.tsx";
 import Buttons from "../views/Buttons.tsx";
+import Home from "../views/Home.tsx";
 
 const routes = (
   <Route path="/" element={<App />}>
-    <Route path="/" element={<div>Home</div>} />
+    <Route path="/" element={<Home />} />
     <Route path="buttons" element={<Buttons />} />
   </Route>
 );
--- src/views/Buttons.tsx
+++ src/views/Buttons.tsx
@@ -1,9 +1,28 @@
 import React from "react";
 
+import { Button } from "@mantine/core";
+
 export default function Buttons() {
   return (
     <div>
       <h1>Buttons</h1>
+
+      <p>Variants</p>
+      <Button style={{ marginRight: "8px" }} variant="subtle">
+        Click me
+      </Button>
+      <Button style={{ marginRight: "8px" }} variant="light">
+        Click me
+      </Button>
+      <Button style={{ marginRight: "8px" }} variant="outline">
+        Click me
+      </Button>
+      <Button style={{ marginRight: "8px" }} variant="filled">
+        Click me
+      </Button>
+      <Button style={{ marginRight: "8px" }} variant="default">
+        Click me
+      </Button>
     </div>
   );
 }
