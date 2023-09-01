cfc2f4337f1985565b36284a8ad049d73a3aa5c0
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-09-01 18:28:54 +1000

    feat: First pass of button coloring scheme

--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -1,3 +1,66 @@
-import { createTheme } from "@mantine/core";
+import {
+  Button,
+  ButtonFactory,
+  PartialVarsResolver,
+  createTheme,
+} from "@mantine/core";
 
-export const theme = createTheme({});
+import buttonClasses from "./css/buttons.module.css";
+
+const buttonVars: PartialVarsResolver<ButtonFactory> = (_theme, props) => {
+  if (props.size === "xs") {
+    return {
+      root: {
+        "--button-fz": "12px",
+        "--button-padding-x": "8px",
+        "--button-height": "24px",
+        "--button-radius": "6px",
+      },
+    };
+  }
+
+  if (props.size === "sm") {
+    return {
+      root: {
+        "--button-fz": "14px",
+        "--button-padding-x": "16px",
+        "--button-height": "32px",
+        "--button-radius": "6px",
+      },
+    };
+  }
+
+  if (props.size === "md") {
+    return {
+      root: {
+        "--button-fz": "14px",
+        "--button-padding-x": "20px",
+        "--button-height": "44px",
+        "--button-radius": "6px",
+      },
+    };
+  }
+
+  if (props.size === "lg") {
+    return {
+      root: {
+        "--button-fz": "16px",
+        "--button-padding-x": "24px",
+        "--button-height": "56px",
+        "--button-radius": "6px",
+      },
+    };
+  }
+
+  return { root: {} };
+};
+
+export const theme = createTheme({
+  focusRing: "always",
+  components: {
+    Button: Button.extend({
+      classNames: buttonClasses,
+      vars: buttonVars,
+    }),
+  },
+});
--- src/views/Buttons.tsx
+++ src/views/Buttons.tsx
@@ -5,24 +5,57 @@ import { Button } from "@mantine/core";
 export default function Buttons() {
   return (
     <div>
-      <h1>Buttons</h1>
+      <div>
+        <h2>Buttons</h2>
+        <h3>Default variants</h3>
+        <Button className="mr-4" variant="subtle">
+          Click me
+        </Button>
+        <Button className="mr-4" variant="light">
+          Click me
+        </Button>
+        <Button className="mr-4" variant="outline">
+          Click me
+        </Button>
+        <Button className="mr-4" variant="filled">
+          Click me
+        </Button>
+        <Button className="mr-4" variant="default">
+          Click me
+        </Button>
+      </div>
 
-      <p>Variants</p>
-      <Button style={{ marginRight: "8px" }} variant="subtle">
-        Click me
-      </Button>
-      <Button style={{ marginRight: "8px" }} variant="light">
-        Click me
-      </Button>
-      <Button style={{ marginRight: "8px" }} variant="outline">
-        Click me
-      </Button>
-      <Button style={{ marginRight: "8px" }} variant="filled">
-        Click me
-      </Button>
-      <Button style={{ marginRight: "8px" }} variant="default">
-        Click me
-      </Button>
+      <div className="mt-8">
+        <h3>GH variants</h3>
+        <Button variant="accent" className="mr-4">
+          Accent
+        </Button>
+        <Button variant="danger" className="mr-4">
+          Danger
+        </Button>
+        <Button variant="warning" className="mr-4">
+          Warning
+        </Button>
+        <Button variant="neutral" className="mr-4">
+          Neutral
+        </Button>
+      </div>
+
+      <div className="mt-8">
+        <h3>Sizes</h3>
+        <Button variant="accent" size="xs" className="mr-4">
+          Label
+        </Button>
+        <Button variant="accent" size="sm" className="mr-4">
+          Small
+        </Button>
+        <Button variant="accent" size="md" className="mr-4">
+          Medium
+        </Button>
+        <Button variant="accent" size="lg" className="mr-4">
+          Large
+        </Button>
+      </div>
     </div>
   );
 }
