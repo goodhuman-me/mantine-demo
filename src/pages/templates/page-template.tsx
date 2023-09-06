0d3c08deaafaea294005d3295e591b935cfc27ed
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-09-07 08:41:29 +1000

    feat: Added page template for ez page creation.

---
+++ src/pages/templates/page-template.tsx
@@ -0,0 +1,14 @@
+import { Title, Text } from "@mantine/core";
+import DefaultLayout from "../../layouts/DefaultLayout";
+import { Content } from "../../components/Content";
+
+export default function PageTemplate() {
+  return (
+    <DefaultLayout title="Page title" subtitle="Sub title here">
+      <Content.Header>Header</Content.Header>
+      <Content.Section title="Section header">
+        <Content.Text>Section text</Content.Text>
+      </Content.Section>
+    </DefaultLayout>
+  );
+}
