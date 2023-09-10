c12f79d243f8e1322d3dc7af0f7c31852449dd82
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-09-10 20:45:21 +1000

    feat: Checkbox & Radio button pages.

--- src/pages/individual/controls/CheckBoxPage.tsx
+++ src/pages/individual/controls/CheckBoxPage.tsx
@@ -1,4 +1,4 @@
-import { Title, Text, Code } from "@mantine/core";
+import { Title, Text, Code, Checkbox, Stack, Box, Group } from "@mantine/core";
 import DefaultLayout from "../../../layouts/DefaultLayout";
 import { Content } from "../../../components/Content";
 
@@ -6,7 +6,79 @@ export default function CheckBoxPage() {
   return (
     <DefaultLayout title="Checkbox" subtitle="Binary select input">
       <Content.Section title="Checkbox">
-        <Content.Text>Binary boolean input</Content.Text>
+        <Content.Text>
+          Basic <Code>Checkbox</Code> usage
+        </Content.Text>
+        <Content.Area>
+          <Checkbox
+            label="By checking this, I agree to finish this bowl of chili without consuming any fluids later."
+            defaultChecked
+            description="I understand that this is a very bad idea."
+          />
+        </Content.Area>
+        <Content.Area>
+          <Checkbox label="Select all rows" indeterminate />
+        </Content.Area>
+      </Content.Section>
+
+      <Content.Section title="Checkbox group">
+        <Content.Text>
+          A group of <Code>Checkbox</Code> components
+        </Content.Text>
+        <Content.Area>
+          <Checkbox.Group label="What is the greatest distortion pedal of all time?" description="Check all that applies" withAsterisk>
+            <Group mt="xs">
+              <Checkbox label="Boss DS-1" />
+              <Checkbox label="Boss MT-2" />
+              <Checkbox label="Proco Rat" />
+              <Checkbox label="Gamechanger Plasma" />
+              <Checkbox label="Ibanez TubeScreamer" />
+              <Checkbox label="My custom pedal made from loose parts of my car" />
+              <Checkbox label="None of these lame boxes" />
+            </Group>
+          </Checkbox.Group>
+        </Content.Area>
+      </Content.Section>
+
+      <Content.Section title="Sizes">
+        <Content.Text>
+          Various <Code>Checkbox</Code> sizes
+        </Content.Text>
+        <Content.Area>
+          <Stack gap="md">
+            <Checkbox description="Cool beans make cool meals." label="x-small" size="xs" defaultChecked />
+            <Checkbox description="Cool beans make cool meals." label="small" size="sm" defaultChecked />
+            <Checkbox description="Cool beans make cool meals." label="medium" size="md" defaultChecked />
+            <Checkbox description="Cool beans make cool meals." label="large" size="lg" defaultChecked />
+            <Checkbox description="Cool beans make cool meals." label="x-large" size="xl" defaultChecked />
+          </Stack>
+        </Content.Area>
+      </Content.Section>
+      <Content.Section title="States">
+        <Content.Text>
+          Different <Code>Checkbox</Code> states
+        </Content.Text>
+        <Content.Area>
+          <Stack gap="lg">
+            <Box>
+              <Text fw="bold" mb="sm">
+                Error state
+              </Text>
+              <Checkbox
+                label="I shot the sheriff."
+                description="But I did not shoot the deputy."
+                error="Oh no! You have to admit your guilt to proceed."
+              />
+            </Box>
+
+            <Box>
+              <Text fw="bold" mb="sm">
+                Disabled state
+              </Text>
+              <Checkbox label="I want to sell my Bitcoin inventory." disabled description="This feature has been temporarily disabled." />
+            </Box>
+          </Stack>
+        </Content.Area>
       </Content.Section>
     </DefaultLayout>
   );
--- src/pages/individual/controls/RadioButtonPage.tsx
+++ src/pages/individual/controls/RadioButtonPage.tsx
@@ -1,16 +1,103 @@
-import { Title, Text, Code } from "@mantine/core";
+import { useState } from "react";
+import { Title, Text, Code, Radio, Stack, Center } from "@mantine/core";
 import DefaultLayout from "../../../layouts/DefaultLayout";
 import { Content } from "../../../components/Content";
 
 export default function RadioButtonPage() {
+  const [value, setValue] = useState("mug");
+
   return (
     <DefaultLayout title="Radio buttons" subtitle="Multiple choice input">
       <Content.Section title="Radio buttons">
         <Content.Text>
-          Stylish wrappers for <Code>radio</Code> elemnents
+          Cool wrappers for <Code>Radio</Code> elements
         </Content.Text>
+        <Content.Area>
+          <Radio label="Opt in for emails" description="I wish to be contacted by the marketing team for promotions" checked />
+        </Content.Area>
+      </Content.Section>
+
+      <Content.Section title="Radio groups">
+        <Content.Text>
+          Grouped <Code>Radio</Code> components
+        </Content.Text>
+        <Content.Area>
+          <Radio.Group
+            label="Please select your starting free gift"
+            description="This will be given to you when you enter the Matrix."
+            value={value}
+            onChange={setValue}
+          >
+            <Stack mt="lg">
+              <Radio value="mug" label="Mug" description="You can store drinks in this." checked />
+              <Radio value="bug" label="Bug" description="Jumps, flies and generally friendly." checked />
+              <Radio value="toy" label="Toy" description="Visit your childhood dreams." checked />
+              <Radio value="mobile" label="Mobile" description="Reception not guaranteed." checked />
+              <Radio value="nothing" label="Nothing" description="Go into the Matrix as you came from it." checked />
+            </Stack>
+          </Radio.Group>
+        </Content.Area>
+      </Content.Section>
+
+      <Content.Section title="Label">
+        <Content.Text>
+          Custom <Code>Radio</Code> labels & descriptions
+        </Content.Text>
+        <Content.Area>
+          <Radio
+            label={
+              <Center bg="pink" c="white" style={{ borderRadius: "var(--mantine-radius-default)" }} p="4px">
+                Chicken & chips
+              </Center>
+            }
+            description={
+              <Center bg="pink.0" p="xs" style={{ borderRadius: "var(--mantine-radius-default)" }}>
+                Always the best pub food, no argument
+              </Center>
+            }
+            checked
+          />
+        </Content.Area>
+      </Content.Section>
+
+      <Content.Section title="Sizes">
+        <Content.Text>
+          Various <Code>Radio</Code> sizes
+        </Content.Text>
+        <Content.Area>
+          <Stack gap="md">
+            <Radio label="x-small" description="Tis but a scratch." checked size="xs" />
+            <Radio label="small" description="Tis but a scratch." checked size="sm" />
+            <Radio label="medium" description="Tis but a scratch." checked size="md" />
+            <Radio label="large" description="Tis but a scratch." checked size="lg" />
+            <Radio label="x-large" description="Tis but a scratch." checked size="xl" />
+          </Stack>
+        </Content.Area>
+      </Content.Section>
+
+      <Content.Section title="States">
+        <Content.Text>
+          Different <Code>Radio</Code> states
+        </Content.Text>
+        <Content.Area>
+          <Text fw="bold" mb="sm">
+            Error state
+          </Text>
+          <Radio
+            label="What is your quest?"
+            description="To seek the Holy Grail."
+            checked
+            error="Silly sod, no such thing as holy grails."
+          />
+        </Content.Area>
+        <Content.Area>
+          <Text fw="bold" mb="sm">
+            Disabled state
+          </Text>
+          <Radio label="I am invincible!" description="No you're not, good sir, you're just a loony." checked disabled />
+        </Content.Area>
       </Content.Section>
     </DefaultLayout>
   );
 }
-// 
+//
