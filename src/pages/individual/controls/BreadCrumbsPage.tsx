5f8afafadde364c92f69d4543599758907e37f4b
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-09-11 19:33:59 +1000

    feat: Added BreadCrumbs page.

--- src/pages/individual/controls/BreadCrumbsPage.tsx
+++ src/pages/individual/controls/BreadCrumbsPage.tsx
@@ -1,18 +1,39 @@
-import { Title, Text, Code, Center } from "@mantine/core";
+import { Title, Text, Code, Center, Breadcrumbs, Anchor } from "@mantine/core";
 import DefaultLayout from "../../../layouts/DefaultLayout";
 import { Content } from "../../../components/Content";
+
+// const items = [
+//   { title: "Mantine", href: "#" },
+//   { title: "Mantine hooks", href: "#" },
+//   { title: "use-id", href: "#" },
+// ].map((item, index) => (
+//   // rome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+//   <Anchor href={item.href} key={index}>
+//     {item.title}
+//   </Anchor>
+// ));
 
 export default function BreadCrumbsPage() {
   return (
     <DefaultLayout title="Breadcrumbs" subtitle="Separates of react nodes with separators">
       <Content.Section title="Breadcrumbs">
-        <Content.Text>Typical breadcrumbs</Content.Text>
+        <Content.Text>
+          Typical <Code>Breadcrumbs</Code> <br />
+          <i>To create sample with dropdown navigators</i>
+        </Content.Text>
         <Content.Area>
-          <Center bg="gray.0" p="3rem" style={{ borderRadius: "1rem" }}>
-            <Text fz="lg" c="gray.6">
-              🚧&nbsp;&nbsp;&nbsp;Coming soon&nbsp;&nbsp;&nbsp;🚧
-            </Text>
-          </Center>
+          <Breadcrumbs separator={<Text c="var(--brand-body-main)">/</Text>}>
+            <Anchor c="var(--brand-accent-main)" href="#wallet">
+              Native Instruments
+            </Anchor>
+            <Anchor c="var(--brand-accent-main)" href="#wallet">
+              Effects
+            </Anchor>
+            <Anchor c="var(--brand-accent-main)" href="#wallet">
+              Delays
+            </Anchor>
+            <Text c="var(--brand-body-dark-1)">Replika XT</Text>
+          </Breadcrumbs>
         </Content.Area>
       </Content.Section>
     </DefaultLayout>
