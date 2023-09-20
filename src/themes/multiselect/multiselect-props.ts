2bf74a06ecc9d94bcf1623b9d1f2e9a7bd501060
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-09-20 17:43:03 +1000

    fix: Fixed error state for MultiSelect

--- src/themes/multiselect/multiselect-props.ts
+++ src/themes/multiselect/multiselect-props.ts
@@ -1,10 +1,9 @@
 import { MultiSelect } from "@mantine/core";
 
 import { inputVars } from "../input/input-vars";
-import inputClasses from "../input/input.module.css";
+import inputClasses from "./multiselect.module.css";
 import { defaultInputProps } from "../input/default-props";
 
-// TODO: Fix Error/disabled states
 // TODO: Style the dropdown
 export const multiselectProps = MultiSelect.extend({
   classNames: inputClasses,
