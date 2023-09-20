3e39c3b381052a85b7a652532dc865842e4513f5
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-09-20 18:07:49 +1000

    feat: Updated TagsInput styles.

--- src/themes/tagsinput/tagsinputs-props.ts
+++ src/themes/tagsinput/tagsinputs-props.ts
@@ -1,14 +1,15 @@
 import { TagsInput } from "@mantine/core";
+import cx from "clsx";
 
 import { inputVars } from "../input/input-vars";
 import inputClasses from "../input/input.module.css";
+import multiSelectClasses from "../multiselect/multiselect.module.css";
 import { defaultInputProps } from "../input/default-props";
 
 // TODO input margin for small size  doesn't look good, need to fix it
-// TODO Error state borders css aren't working
 // TODO Disabled state css isn't working
 export const tagsInputProps = TagsInput.extend({
-  classNames: inputClasses,
+  classNames: { ...inputClasses, input: cx(inputClasses.input, multiSelectClasses.input) },
   defaultProps: defaultInputProps,
   vars: inputVars,
 });
