3a8f239e70be2c639e3a39a237aef5fd3496cc6f
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-09-20 18:06:02 +1000

    fix: Consolidated multiselect with input css module

--- src/themes/multiselect/multiselect-props.ts
+++ src/themes/multiselect/multiselect-props.ts
@@ -1,12 +1,16 @@
 import { MultiSelect } from "@mantine/core";
+import cx from "clsx";
 
 import { inputVars } from "../input/input-vars";
-import inputClasses from "./multiselect.module.css";
+import inputClasses from "../input/input.module.css";
+
+import multiSelectClasses from "./multiselect.module.css";
+
 import { defaultInputProps } from "../input/default-props";
 
 // TODO: Style the dropdown
 export const multiselectProps = MultiSelect.extend({
-  classNames: inputClasses,
+  classNames: { ...inputClasses, input: cx(inputClasses.input, multiSelectClasses.input) },
   defaultProps: defaultInputProps,
   vars: inputVars,
 });
