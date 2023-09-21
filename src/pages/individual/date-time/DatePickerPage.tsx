0802a4f3077c48f1dd0a7a8114698efc4a3b7ff7
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-09-21 15:25:34 +1000

    feat: Updated DatePicker

--- src/pages/individual/date-time/DatePickerPage.tsx
+++ src/pages/individual/date-time/DatePickerPage.tsx
@@ -1,20 +1,65 @@
-import { Title, Text, Code, Center } from "@mantine/core";
+import { Title, Text, Code, Center, Box, Stack, Group, Select, MantineSize } from "@mantine/core";
 import DefaultLayout from "../../../layouts/DefaultLayout";
 import { Content } from "../../../components/Content";
+import { DatePicker } from "@mantine/dates";
+import { useState } from "react";
 
 export default function DatePickerPage() {
+  const [calendarSize, setCalendarSize] = useState<string | null>("md");
+
   return (
     <DefaultLayout title="DatePicker" subtitle="Inline date, multiple dates and dates range picker">
-      {/* <Content.Header>Header</Content.Header> */}
-      <Content.Section title="Under construction">
-        <Content.Text>Typical usage</Content.Text>
+      <Content.Section title="Basic DatePicker usage">
+        <Content.Text>
+          Typical <Code>DatePicker</Code> usage
+        </Content.Text>
         <Content.Area>
-          <Center bg="gray.0" p="3rem" style={{ borderRadius: "1rem" }}>
-            <Text fz="lg" c="gray.6">
-              🚧&nbsp;&nbsp;&nbsp;Coming soon&nbsp;&nbsp;&nbsp;🚧
-            </Text>
-          </Center>
+          <DatePicker />
         </Content.Area>
+      </Content.Section>
+
+      <Content.Section title="Various options">
+        <Content.Text>
+          Different options for <Code>DatePicker</Code>
+        </Content.Text>
+
+        <Group gap="xl">
+          <Box className="text-center">
+            <Text mb="sm">Allow multi selects</Text>
+            <DatePicker type="multiple" />
+          </Box>
+          <Box className="text-center">
+            <Text mb="sm">Select by range</Text>
+            <DatePicker type="range" />
+          </Box>
+          <Box className="text-center">
+            <Text mb="sm">Hide days not in month</Text>
+            <DatePicker type="range" hideOutsideDates />
+          </Box>
+        </Group>
+      </Content.Section>
+
+      <Content.Section title="Sizes">
+        <Content.Text>
+          Various sizes for <Code>DatePicker</Code>
+        </Content.Text>
+
+        <Stack gap="xl" align="flex-start">
+          <Select
+            value={calendarSize}
+            data={[
+              // "xs", "sm", "md", "lg", "xl"
+              { value: "xs", label: "x-small (xs)" },
+              { value: "sm", label: "small (sm)" },
+              { value: "md", label: "medium (md)" },
+              { value: "lg", label: "large (lg)" },
+              { value: "xl", label: "x-large (xl)" },
+            ]}
+            onChange={setCalendarSize}
+            allowDeselect={false}
+          />
+          <DatePicker size={calendarSize as MantineSize} />
+        </Stack>
       </Content.Section>
     </DefaultLayout>
   );
