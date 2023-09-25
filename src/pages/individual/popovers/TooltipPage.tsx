029a89a51513f9f065610eff12ed820f8e28b366
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-09-25 15:41:12 +1000

    feat: Added Tooltip page.

--- src/pages/individual/popovers/TooltipPage.tsx
+++ src/pages/individual/popovers/TooltipPage.tsx
@@ -1,19 +1,40 @@
-import { Title, Text, Code, Center } from "@mantine/core";
+import { Title, Text, Code, Center, Button, Tooltip, Group } from "@mantine/core";
 import DefaultLayout from "../../../layouts/DefaultLayout";
 import { Content } from "../../../components/Content";
 
 export default function TooltipPage() {
   return (
-    <DefaultLayout title="Page" subtitle="Subtitle">
+    <DefaultLayout title="Tooltip" subtitle="Renders tooltip at given element on mouse over or other event">
       {/* <Content.Header>Header</Content.Header> */}
-      <Content.Section title="Sectionheader">
-        <Content.Text>Typical usage</Content.Text>
+      <Content.Section title="Tooltip">
+        <Content.Text>
+          Typical <Code>Tooltip</Code> usage
+        </Content.Text>
         <Content.Area>
-          <Center bg="gray.0" p="3rem" style={{ borderRadius: "1rem" }}>
-            <Text fz="lg" c="gray.6">
-              🚧&nbsp;&nbsp;&nbsp;Coming soon&nbsp;&nbsp;&nbsp;🚧
-            </Text>
-          </Center>
+          <Tooltip label="Enter the matrix" withArrow>
+            <Button>Pick up call</Button>
+          </Tooltip>
+        </Content.Area>
+      </Content.Section>
+
+      <Content.Section title="Tooltip options">
+        <Content.Text>Tooltip can be placed at different positions aroud the target element</Content.Text>
+
+        <Content.Area>
+          <Group gap="md">
+            <Tooltip label="Top" position="top" withArrow>
+              <Button variant="outline">Top</Button>
+            </Tooltip>
+            <Tooltip label="Right" position="right" withArrow>
+              <Button variant="outline">Right</Button>
+            </Tooltip>
+            <Tooltip label="Bottom" position="bottom" withArrow>
+              <Button variant="outline">Bottom</Button>
+            </Tooltip>
+            <Tooltip label="Left" position="left" withArrow>
+              <Button variant="outline">Left</Button>
+            </Tooltip>
+          </Group>
         </Content.Area>
       </Content.Section>
     </DefaultLayout>
