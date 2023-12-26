9f6f4f89b67b4958eae2a4c5c618dc6ccc609ca6
Jir Wong <97641518+gh-jir@users.noreply.github.com>
2023-12-27 10:12:56 +1100

    feat: Updated sso test page with more testing features

--- apps/mantine-app/src/firebase-app/config.ts
+++ apps/mantine-app/src/firebase-app/config.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { SAMLAuthProvider } from "firebase/auth";
+import { SAMLAuthProvider, getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,5 +14,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
+export const firebaseAuth = getAuth(firebaseApp);
 
-export const samlProvider = new SAMLAuthProvider("saml.jumpcloud-test11-saml");
+export const jumpCloudProvider = new SAMLAuthProvider("saml.jumpcloud-test11-saml");
+export const entraIdProvider = new SAMLAuthProvider("saml.entraid-test11-saml");
--- apps/mantine-app/src/pages/other/TestSSOPage.tsx
+++ apps/mantine-app/src/pages/other/TestSSOPage.tsx
@@ -1,34 +1,56 @@
 import { useEffect, useState } from 'react';
-import { TextInput, Title, Text, Code, Center, Button, Group, Box, Stack } from '@good/ui/core';
+import { TextInput, Text, Button, Group, Box, Stack, Divider, Grid, CopyButton, ButtonGroup } from '@good/ui/core';
 
-import { getAuth, getRedirectResult, signInWithEmailAndPassword, signInWithRedirect } from 'firebase/auth';
+import { User, getRedirectResult, signInWithEmailAndPassword, signInWithRedirect } from 'firebase/auth';
 
 import DefaultLayout from '../../layouts/DefaultLayout';
 import { Content } from '../../components/Content';
 
-import { firebaseApp, samlProvider } from '../../firebase-app/config';
+import { entraIdProvider, firebaseAuth, jumpCloudProvider } from '../../firebase-app/config';
+
+const Row = ({ name, value }: { name: string; value?: string | null }) => (
+	<Grid>
+		<Grid.Col span={2}>{name}</Grid.Col>
+		<Grid.Col span={10}>{value}</Grid.Col>
+	</Grid>
+);
 
 export default function SSOTestPage() {
 	const [email, setEmail] = useState<string>('jirwong@yahoo.com');
 	const [password, setPassword] = useState<string>('HappyMoonHunts761!');
 	const [messages, setMessages] = useState<string[]>([]);
+	const [currentUser, setCurrentUser] = useState<User | null>(null);
+	const [idToken, setIdToken] = useState<string | undefined>('');
 
-	const auth = getAuth(firebaseApp);
+	// const auth = getAuth(firebaseApp);
+	firebaseAuth.onAuthStateChanged(async (user) => {
+		setCurrentUser(user);
+		const idToken = await user?.getIdToken();
+		setIdToken(idToken);
+
+		if (user) {
+			setCurrentUser(user);
+			const idToken = await user?.getIdToken();
+			setIdToken(idToken);
+		}
+	});
 
 	async function loginWithFirebase() {
 		try {
-			const res = await signInWithEmailAndPassword(auth, email, password);
-			// console.log(res.user.email);
+			const res = await signInWithEmailAndPassword(firebaseAuth, email, password);
 			appendMessage(`Logged in as ${res.user.email}`);
-
 			console.log(JSON.stringify(res));
 		} catch (error) {
 			appendMessage((error as Error).message);
 		}
 	}
 
-	async function loginWithSAML() {
-		signInWithRedirect(auth, samlProvider);
+	async function loginWithJumpCloud() {
+		signInWithRedirect(firebaseAuth, jumpCloudProvider);
+	}
+
+	async function loginWithEntraId() {
+		signInWithRedirect(firebaseAuth, entraIdProvider);
 	}
 
 	function appendMessage(message: string) {
@@ -36,12 +58,22 @@ export default function SSOTestPage() {
 	}
 
 	async function displayRedirectStatus() {
-		const result = await getRedirectResult(auth);
+		const result = await getRedirectResult(firebaseAuth);
 		console.log(JSON.stringify(result));
 	}
 
+	async function displayCurrentUser() {
+		console.dir(firebaseAuth.currentUser);
+		// console.log(JSON.stringify(firebaseAuth.currentUser));
+	}
+
+	async function logoutUser() {
+		const res = await firebaseAuth.signOut();
+		console.log(res);
+	}
+
 	useEffect(() => {
-		displayRedirectStatus();
+		// displayRedirectStatus();
 	});
 
 	return (
@@ -68,13 +100,28 @@ export default function SSOTestPage() {
 					/>
 					<Group>
 						<Button onClick={loginWithFirebase}>Login with Firebase</Button>
-						<Button onClick={loginWithSAML}>Login with Jumpcloud</Button>
+						<Button onClick={loginWithJumpCloud}>Login with Jumpcloud</Button>
+						<Button onClick={loginWithEntraId}>Login with EntraID</Button>
 					</Group>
 				</Box>
 			</Content.Section>
 
 			<Box mt='lg' mb='lg'>
-				<Content.Header>Status</Content.Header>
+				<Content.Header>Login information</Content.Header>
+
+				<Box mb='xl'>
+					<Text fw='bold' size='lg' mb='lg'>
+						Login status
+					</Text>
+					<Stack gap='0.2rem'>
+						<Row name='Am I logged in?' value={currentUser !== null ? '✅ Yes' : '❌ No'} />
+						<Row name='uid' value={currentUser?.uid} />
+						<Row name='Display name' value={currentUser?.displayName} />
+						<Row name='Email' value={currentUser?.email} />
+						<Row name='Is anonymous?' value={currentUser?.isAnonymous.toString()} />
+					</Stack>
+				</Box>
+
 				<Box mb='xl'>
 					<Text fw='bold' size='lg' mb='lg'>
 						Utilities
@@ -84,19 +131,31 @@ export default function SSOTestPage() {
 						<Button onClick={displayRedirectStatus} variant='outline'>
 							Show redirect status
 						</Button>
-						{/* sdsdf */}
+						<Button onClick={displayCurrentUser} variant='outline'>
+							Get current user
+						</Button>
+						<Button onClick={logoutUser} variant='outline'>
+							Logout user
+						</Button>
 					</Group>
 				</Box>
 
 				<Box mb='xl'>
 					<Text fw='bold' size='lg' mb='lg'>
-						SAML login status
+						accessToken
 					</Text>
-					<Box p='lg' bg='blue.0' className='rounded-xl'>
-						<Text size='0.9rem' c='gray.6'>
-							Checking status...
+					<Box p='lg' bg='blue.0' className='rounded-xl text-wrap break-all'>
+						<Text size='0.9rem' c='gray.6' className='text-wrap' mb='md'>
+							{idToken}
 						</Text>
 					</Box>
+					<CopyButton value={idToken ?? ''}>
+						{({ copy }) => (
+							<Button mt='md' onClick={copy} disabled={!idToken}>
+								Copy token
+							</Button>
+						)}
+					</CopyButton>
 				</Box>
 
 				<Text fw='bold' size='lg' mb='lg'>
